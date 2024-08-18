1ed76ce2431fa961d0b9e5b45a391294c7d7c6b3
flexOfficial20 <155304501+flexOfficial20@users.noreply.github.com>
2024-08-18 15:22:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -31,8 +31,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
-    "UPSTREAM_REPO", "https://github.com/flexOfficial20/Z-X-C-MUSIC-test",
-)
+    "UPSTREAM_REPO", "https://github.com/flexOfficial20/Z-X-C-MUSIC-test")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
